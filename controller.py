60701d06526bc1d4388d5148dd403fae38d8f6df
SimonsRuben <60577998+SimonsRuben@users.noreply.github.com>
2021-05-01 16:59:55 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -20,10 +20,47 @@ GPIO.setup(lednummers[0],GPIO.OUT)
 
 
 
-def sentmsg(msg):
-	client.publish("ap/groep5", str(msg), qos=0)
 
-num = random.randint(00,100)
+
+
+def on_publish(client, userdata, mid):
+    print("message: "+str(mid))
+
+def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+    #print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
+    client.subscribe("ap/groep5")  # Subscribe to the topic “digitest/test1”, receive any messages published on it
+
+def on_message2(client, userdata,msg):
+	temp = str(msg.payload)
+	print(temp[2])
+	if temp[2] == "$":
+		ledjob = Thread(target = c1.newball)
+		ledjob.start()
+
+
+def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+    #print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
+    temp = str(msg.payload)
+    print(temp)
+    if len(temp) > 7 and c1.id == "-1":
+    	responsenum = temp[2:5]
+    	authnum = "!" + str(c1.num)
+    	if authnum == responsenum:
+    		idnum = temp.split("ID=")[1][0]
+    		print("authenticated ID=" + str(idnum))
+    		c1.setid(idnum)
+    		client.on_message = on_message2
+
+    	else:
+    		print("could not authenticate")
+
+
+client = mqtt.Client(clean_session=True) #make id for mqtt
+client.on_publish = on_publish
+client.on_connect = on_connect  # Define callback function for successful connection
+client.on_message = on_message  # Define callback function for receipt of a message
+client.connect("broker.mqttdashboard.com", 1883) #choose broker and port
+client.loop_start()
 
 class Control():
 	"""docstring for control"""
@@ -31,6 +68,15 @@ class Control():
 		controller.id = idnummer
 		controller.speed = 1
 		controller.leds =leds
+		controller.num = random.randint(00,100)
+		controller.authenticate()
+
+	def authenticate(c):
+		sleep(5)
+		if c.num <10:
+			c.num = "0" + str(c.num)
+		msg="?" + str(c.num)
+		client.publish("ap/groep5",str(msg), qos=0)	
 
 	def setid(c,num):
 		c.id = int(num)
@@ -39,6 +85,16 @@ class Control():
 
 		elif c.id ==1:
 			print("rechte led" + str(c.leds[c.id]))
+	def newball(c):
+		for x in range(3):
+			print("led aan")
+			sleep(0.5)
+			print("led uit")
+			sleep(0.5)
+
+	def sentmsg(c,msg):
+		client.publish("ap/groep5", str(msg), qos=0)
+			
 
 
 	def drukknop(c,channel):
@@ -60,7 +116,7 @@ class Control():
 		#print(msg)
 		if c.id !="-1" and channel != 24:
 
-			job = Thread(target = sentmsg,args=(msg,))
+			job = Thread(target = c1.sentmsg,args=(msg,))
 			job.start()
 			#client.publish("ap/groep5", str(msg), qos=0)
 
@@ -70,61 +126,15 @@ class Control():
 		
 c1 = Control("-1",lednummers)
 
-def on_publish(client, userdata, mid):
-    print("message: "+str(mid))
 
-def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
-    #print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-    client.subscribe("ap/groep5")  # Subscribe to the topic “digitest/test1”, receive any messages published on it
-
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
-    #print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
-    temp = str(msg.payload)
-    print(temp)
-    if len(temp) > 7 and c1.id == "-1":
-    	responsenum = temp[2:5]
-    	authnum = "!" + str(num)
-    	if authnum == responsenum:
-    		idnum = temp.split("ID=")[1][0]
-    		print("authenticated ID=" + str(idnum))
-    		c1.setid(idnum)
-
-    	else:
-    		print("could not authenticate")
-
-
-client = mqtt.Client(clean_session=True) #make id for mqtt
-client.on_publish = on_publish
-client.on_connect = on_connect  # Define callback function for successful connection
-client.on_message = on_message  # Define callback function for receipt of a message
-client.connect("broker.mqttdashboard.com", 1883) #choose broker and port
-client.loop_start()
-
-def druk(channel):
-	c1.drukknop(channel)
-	
-	#print(msg)
-
-GPIO.add_event_detect(23, GPIO.RISING, callback = druk, bouncetime=300)
-GPIO.add_event_detect(24, GPIO.RISING, callback = druk, bouncetime=300)
-GPIO.add_event_detect(25, GPIO.RISING, callback = druk, bouncetime=300)
-
-
-def authenticate():
-	global num
-	sleep(5)
-	if num <10:
-		num = "0" + str(num)
-	msg="?" + str(num)
-	client.publish("ap/groep5",str(msg), qos=0)
+GPIO.add_event_detect(23, GPIO.RISING, callback = c1.drukknop, bouncetime=300)
+GPIO.add_event_detect(24, GPIO.RISING, callback = c1.drukknop, bouncetime=300)
+GPIO.add_event_detect(25, GPIO.RISING, callback = c1.drukknop, bouncetime=300)
 
 
 try:
-	print("start authenticate")
-	authenticate()
-	
 	while True:
-		sleep(0.05)
+		sleep(5)
 
 except KeyboardInterrupt:
 	client.loop_stop()
